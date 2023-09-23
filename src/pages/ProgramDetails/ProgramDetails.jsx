6e75e0a0e9f4ddc84edab850d20f40a69f63554c
Yurii PErekrestnyi <jodlei1618@gmail.com>
2023-09-23 23:05:51 +0300

    fix video component

--- src/pages/ProgramDetails/ProgramDetails.jsx
+++ src/pages/ProgramDetails/ProgramDetails.jsx
@@ -3,19 +3,19 @@ import AllProgramInfo from "../../Components/AllProgramInfo/AllProgramInfo";
 import ExerciceList from "../../Components/ExerciceList/ExerciceList";
 import ProgramTopPanel from "../../Components/ProgramTopPanel/ProgramTopPanel";
 import Container from "../../Components/ui/Container/Container.styled";
-import {useProgram} from "../../hooks/useProgram.jsx";
-import {useEffect} from "react";
-import {detailProgram} from "../../redux/program/operations.js";
-import {useDispatch} from "react-redux";
+import { useProgram } from "../../hooks/useProgram.jsx";
+import { useEffect } from "react";
+import { detailProgram } from "../../redux/program/operations.js";
+import { useDispatch } from "react-redux";
 
 const ProgramDetails = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
-  const { isLoading, programDetails } = useProgram();
+  const { programDetails } = useProgram();
 
   useEffect(() => {
-      dispatch(detailProgram(id));
-  }, [dispatch]);
+    dispatch(detailProgram(id));
+  }, [dispatch, id]);
 
   const location = useLocation();
   const backLinkHref = location.state?.from ?? "/";
