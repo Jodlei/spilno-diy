530e152f074bcfe68a62553bf0e44227d44148ad
Yurii PErekrestnyi <jodlei1618@gmail.com>
2023-09-24 22:08:35 +0300

    add loader, fix operatin refresh user, fix storage with persist

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,106 +1,105 @@
-import {createSlice} from "@reduxjs/toolkit";
-import {login, logOut, refreshUser, register} from "./operations";
-import {toast} from "react-toastify";
+import { createSlice } from "@reduxjs/toolkit";
+import { login, logOut, refreshUser, register } from "./operations";
+import { toast } from "react-toastify";
 import storage from "../../services/storage";
 
 const initialState = {
-    user: {name: null, email: null},
-    token:  null,
-    isLoggedIn: false,
-    isRefreshing: true,
-    isLoading: false,
-    isLoadingGoogle: false,
-    status: false,
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: true,
+  isLoading: false,
+  status: false,
 };
 
 export const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) =>
-        builder
-            .addCase(register.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(register.fulfilled, (state, action) => {
-                state.status = true;
-                state.isLoading = false;
-                toast.success("Будь-ласка підтвердіть електронну пошту");
-            })
-            .addCase(register.rejected, (state, action) => {
-                toast.error(action.payload);
-                state.isLoading = false;
-            })
-            .addCase(login.pending, (state) => {
-                state.isLoading = true;
-                state.status = false;
-            })
-            .addCase(login.fulfilled, (state, action) => {
-                state.user = action.payload.user;
-                state.token = action.payload.token;
-                storage.setToken(state.token);
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) =>
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.status = true;
+        state.isLoading = false;
+        toast.success("Будь-ласка підтвердіть електронну пошту");
+      })
+      .addCase(register.rejected, (state, action) => {
+        toast.error(action.payload);
+        state.isLoading = false;
+      })
+      .addCase(login.pending, (state) => {
+        state.isLoading = true;
+        state.status = false;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
 
-                state.isLoggedIn = true;
-                state.isLoading = false;
-                state.status = false;
-            })
-            .addCase(login.rejected, (state, action) => {
-                toast.error(action.payload);
-                state.isLoading = false;
-                state.status = false;
-            })
-            .addCase(refreshUser.pending, (state) => {
-                state.isRefreshing = true;
-            })
-            .addCase(refreshUser.fulfilled, (state, action) => {
-                state.user = action.payload.user;
+        // storage.setToken(state.token);
 
-                state.isLoggedIn = true;
-                state.isRefreshing = false;
+        state.isLoggedIn = true;
+        state.isLoading = false;
+        state.status = false;
+      })
+      .addCase(login.rejected, (state, action) => {
+        toast.error(action.payload);
+        state.isLoading = false;
+        state.status = false;
+      })
+      .addCase(refreshUser.pending, (state) => {
+        state.isRefreshing = true;
+      })
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload.user;
 
-                storage.set('userData', JSON.stringify(state.user));
-                storage.set('isLogged', state.isLoggedIn);
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
 
-            })
-            .addCase(refreshUser.rejected, (state) => {
-                state.isRefreshing = false;
+        storage.set("userData", JSON.stringify(state.user));
+        storage.set("isLogged", state.isLoggedIn);
+      })
+      .addCase(refreshUser.rejected, (state) => {
+        state.isRefreshing = false;
 
-                storage.removeItem('userData');
-                storage.removeItem('isLogged');
-            })
+        storage.removeItem("userData");
+        storage.removeItem("isLogged");
+      })
 
-            .addCase(logOut.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(logOut.fulfilled, (state) => {
-                state.user = {name: null, email: null, avatarURL: null};
-                state.isLoggedIn = false;
-                state.token = null;
-                state.isLoading = false;
+      .addCase(logOut.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(logOut.fulfilled, (state) => {
+        state.user = { name: null, email: null, avatarURL: null };
+        state.isLoggedIn = false;
+        state.token = null;
+        state.isLoading = false;
 
-                storage.removeToken();
-                storage.removeItem('userData');
-                storage.removeItem('isLogged');
-            })
-            .addCase(logOut.rejected, (state) => {
-                state.isRefreshing = false;
-                state.isLoading = false;
+        storage.removeToken();
+        storage.removeItem("userData");
+        storage.removeItem("isLogged");
+      })
+      .addCase(logOut.rejected, (state) => {
+        state.isRefreshing = false;
+        state.isLoading = false;
 
-                storage.removeToken();
-                storage.removeItem('userData');
-                storage.removeItem('isLogged');
-            })
+        storage.removeToken();
+        storage.removeItem("userData");
+        storage.removeItem("isLogged");
+      })
 
-            .addDefaultCase((state, action) => {
-                if (action.error) {
-                    if (action.payload === "Not authorized") {
-                        state.isLoggedIn = false;
-                        state.token = null;
-                    }
-                    state.error = action.payload;
-                    toast.error(action.payload);
-                }
-            }),
+      .addDefaultCase((state, action) => {
+        if (action.error) {
+          if (action.payload === "Not authorized") {
+            state.isLoggedIn = false;
+            state.token = null;
+          }
+          state.error = action.payload;
+          toast.error(action.payload);
+        }
+      }),
 });
 
 export const authReducer = authSlice.reducer;
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,85 +1,95 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { authorize,
-    getAuthUser,
-    registerUser,
-    resetPassword,
-    logout,
-    getSocialRedirectUrl,
-    socialLogin,
-    confirmEmail } from "../../api/auth";
+import {
+  authorize,
+  getAuthUser,
+  registerUser,
+  resetPassword,
+  logout,
+  getSocialRedirectUrl,
+  socialLogin,
+  confirmEmail,
+} from "../../api/auth";
+import axios from "axios";
 
-export const register = createAsyncThunk('auth/register', async (data) => {
-    const response = await registerUser(data);
-    return response.data
-})
+export const instance = axios.create({
+  baseURL: "https://spilno-diy-back.itbox.com.ua",
+});
 
-export const login = createAsyncThunk(
-    "auth/login",
-    async ( data) => {
+const setToken = (token) => {
+  if (token) {
+    return (instance.defaults.headers.authorization = `Bearer ${token}`);
+  }
+  instance.defaults.headers.authorization = "";
+};
 
-        const response = await authorize(data);
-        return response.data
-    }
-);
+export const register = createAsyncThunk("auth/register", async (data) => {
+  const response = await registerUser(data);
+  return response.data;
+});
 
+export const login = createAsyncThunk("auth/login", async (data) => {
+  const response = await authorize(data);
+
+  setToken(response.data.token);
+
+  return response.data;
+});
 
 export const resetPasswordSend = createAsyncThunk(
-    "auth/resetPassword",
-    async ( data) => {
-
-        const response = await resetPassword(data);
-        return response.data
-    }
+  "auth/resetPassword",
+  async (data) => {
+    const response = await resetPassword(data);
+    return response.data;
+  }
 );
-
-
-
 
 export const refreshUser = createAsyncThunk(
-    "auth/auth_user",
-    async () => {
-        const response = await getAuthUser();
-        return response.data
+  "auth/auth_user",
+  async (_, thunkAPI) => {
+    const { token } = thunkAPI.getState().auth;
+    if (!token) {
+      return thunkAPI.rejectWithValue("Unable to fetch user");
     }
+    setToken(token);
+    try {
+      const resp = await instance.get("/api/v1/auth/me");
+      return resp.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response.data.message);
+    }
+  }
 );
 
+// export const refreshUser = createAsyncThunk("auth/auth_user", async () => {
+//   const response = await getAuthUser();
+//   return response.data;
+// });
+
 export const sendSocialLogin = createAsyncThunk(
-    "auth/socialLogin",
-    async ( data) => {
-
-
-        const response = await socialLogin(data);
-        return response.data
-    }
+  "auth/socialLogin",
+  async (data) => {
+    const response = await socialLogin(data);
+    return response.data;
+  }
 );
 
 export const sendConfirmEmail = createAsyncThunk(
-    "auth/confirmEmail",
-    async ( data) => {
-
-
-        const response = await confirmEmail(data);
-        return response.data
-    }
+  "auth/confirmEmail",
+  async (data) => {
+    const response = await confirmEmail(data);
+    return response.data;
+  }
 );
 
-export const logOut = createAsyncThunk(
-    "auth/logout",
-    async ( data) => {
-
-
-        const response = await logout();
-        return response.data
-    }
-);
+export const logOut = createAsyncThunk("auth/logout", async (data) => {
+  const response = await logout();
+  return response.data;
+});
 
 export const getSocialRedirectUrlSend = createAsyncThunk(
-    "auth/getSocialLogin",
-    async ( data) => {
-
-
-        const response = await getSocialRedirectUrl(data);
-        return response.data
-    }
+  "auth/getSocialLogin",
+  async (data) => {
+    const response = await getSocialRedirectUrl(data);
+    return response.data;
+  }
 );
-
